003027ef6cf249ced9a109775ab1fd448bdbeac9
Bres1990 <adam.poteralowicz@gmail.com>
2020-10-18 20:26:38 +0200

    Setup HomeCommandCenter project

---
+++ app/src/main/java/com/apap/hcc/FullscreenActivity.kt
@@ -0,0 +1,96 @@
+package com.apap.hcc
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.os.Handler
+import android.view.MotionEvent
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+open class FullscreenActivity : AppCompatActivity() {
+    private lateinit var fullscreenContent: TextView
+    private lateinit var fullscreenContentControls: LinearLayout
+    private val hideHandler = Handler()
+
+    @SuppressLint("InlinedApi")
+    private val hidePart2Runnable = Runnable {
+        fullscreenContent.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                        View.SYSTEM_UI_FLAG_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    }
+
+    private val showPart2Runnable = Runnable {
+        supportActionBar?.show()
+        fullscreenContentControls.visibility = View.VISIBLE
+    }
+
+    private var isFullscreen: Boolean = false
+    private val hideRunnable = Runnable { hide() }
+
+    private val delayHideTouchListener = View.OnTouchListener { view, motionEvent ->
+        when (motionEvent.action) {
+            MotionEvent.ACTION_DOWN -> if (AUTO_HIDE) {
+                delayedHide(AUTO_HIDE_DELAY_MILLIS)
+            }
+            MotionEvent.ACTION_UP -> view.performClick()
+        }
+        false
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        isFullscreen = true
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        delayedHide(100)
+    }
+
+    private fun toggle() {
+        if (isFullscreen) {
+            hide()
+        } else {
+            show()
+        }
+    }
+
+    private fun hide() {
+        supportActionBar?.hide()
+        fullscreenContentControls.visibility = View.GONE
+        isFullscreen = false
+
+        hideHandler.removeCallbacks(showPart2Runnable)
+        hideHandler.postDelayed(hidePart2Runnable, UI_ANIMATION_DELAY.toLong())
+    }
+
+    private fun show() {
+        fullscreenContent.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        isFullscreen = true
+
+        hideHandler.removeCallbacks(hidePart2Runnable)
+        hideHandler.postDelayed(showPart2Runnable, UI_ANIMATION_DELAY.toLong())
+    }
+
+    private fun delayedHide(delayMillis: Int) {
+        hideHandler.removeCallbacks(hideRunnable)
+        hideHandler.postDelayed(hideRunnable, delayMillis.toLong())
+    }
+
+    companion object {
+        private const val AUTO_HIDE = true
+        private const val AUTO_HIDE_DELAY_MILLIS = 3000
+        private const val UI_ANIMATION_DELAY = 300
+    }
+}
---
+++ app/src/main/java/com/apap/hcc/HomeActivity.kt
@@ -0,0 +1,15 @@
+package com.apap.hcc
+
+import android.os.Bundle
+
+class HomeActivity: FullscreenActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+
+    }
+}
