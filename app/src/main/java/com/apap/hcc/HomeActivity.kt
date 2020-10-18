f0092fb7038c3d0fabf2129afd08f5608b260af8
Bres1990 <adam.poteralowicz@gmail.com>
2020-10-18 22:31:01 +0200

    Setup BottomNavigationView in HomeActivity

--- app/src/main/java/com/apap/hcc/HomeActivity.kt
+++ app/src/main/java/com/apap/hcc/HomeActivity.kt
@@ -1,12 +1,24 @@
 package com.apap.hcc
 
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity: FullscreenActivity() {
 
+    private val homeFragment = supportFragmentManager.findFragmentById(R.id.home_fragment) as NavHostFragment
+    private val navController = homeFragment.navController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupBottomNavigation()
         setupListeners()
+    }
+
+    private fun setupBottomNavigation() {
+        findViewById<BottomNavigationView>(R.id.home_bottom_navigation)
+            .setupWithNavController(navController)
     }
 
     private fun setupListeners() {
