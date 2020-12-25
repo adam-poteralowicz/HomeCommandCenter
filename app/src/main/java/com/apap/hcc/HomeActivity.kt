f363f71270cd6cdd636fdf3517dc873798b363f5
Bres1990 <adam.poteralowicz@gmail.com>
2020-12-25 23:08:11 +0100

    Setup binding for HomeActivity

--- app/src/main/java/com/apap/hcc/HomeActivity.kt
+++ app/src/main/java/com/apap/hcc/HomeActivity.kt
@@ -1,19 +1,29 @@
 package com.apap.hcc
 
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.apap.hcc.databinding.ActivityHomeBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity: FullscreenActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
 
     private val homeFragment = supportFragmentManager.findFragmentById(R.id.home_fragment) as NavHostFragment
     private val navController = homeFragment.navController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupBinding()
         setupBottomNavigation()
         setupListeners()
+    }
+
+    private fun setupBinding() {
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     private fun setupBottomNavigation() {
@@ -22,6 +32,24 @@ class HomeActivity: FullscreenActivity() {
     }
 
     private fun setupListeners() {
+        with(binding) {
+            homeBottomNavigation.setOnNavigationItemSelectedListener {
+                binding.homeBottomNavigation.itemBackground = ContextCompat.getDrawable(
+                    this@HomeActivity,
+                    android.R.color.holo_orange_dark
+                )
 
+                when (it.itemId) {
+                    R.id.home_fragment -> showTitle(getString(R.string.home_nav_text))
+                    R.id.calendar_fragment -> showTitle(getString(R.string.calendar_nav_text))
+                    R.id.notes_fragment -> showTitle(getString(R.string.notes_nav_text))
+                }
+                true
+            }
+        }
+    }
+
+    private fun showTitle(text: String) {
+        binding.title.text = text
     }
 }
